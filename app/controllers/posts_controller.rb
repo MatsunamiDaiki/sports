cb4b6f9077b88d9b401294a3217a85776e33a625
MatsunamiDaiki <lax.md0@gmail.com>
2020-09-08 18:58:32 +0900

    検索機能実装！！

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -40,6 +40,10 @@ class PostsController < ApplicationController
       redirect_to root_path
   end
 
+  def search
+    @posts = Post.search(params[:keyword])
+  end
+
   private
   def post_params
     params.require(:post).permit(:title,:text,:genre_id).merge(user_id: current_user.id)
